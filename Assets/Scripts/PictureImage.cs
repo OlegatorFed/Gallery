618fbac7ab5d4697d473b204969007d4cef5bb86
Oleg <fed.olegator1@gmail.com>
2021-05-23 09:59:25 +1000

    Added full screen button

--- Assets/Scripts/PictureImage.cs
+++ Assets/Scripts/PictureImage.cs
@@ -5,6 +5,15 @@ using UnityEngine.UI;
 
 public class PictureImage : MonoBehaviour
 {
+
+    public GameObject unFullScreenBtn;
+    public GameObject fullScreenBtn;
+    public GameObject exitBtn;
+
+    Vector2 maxOffsetUnFull = new Vector2(1000f, 0);
+    Vector2 minOffsetUnFull = new Vector2(0, -450f);
+    Vector2 halfVec = new Vector2(0.5f, 0.5f);
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,5 +33,40 @@ public class PictureImage : MonoBehaviour
         GetComponent<Image>().sprite = sprite;
 
         gameObject.SetActive(true);
+    }
+
+    public void GoFullScreen()
+    {
+        RectTransform rect = GetComponent<Image>().rectTransform;
+
+        rect.anchorMin = Vector2.zero;
+        rect.anchorMax = Vector2.one;
+        rect.pivot = halfVec;
+        rect.offsetMax = Vector2.zero;
+        rect.offsetMin = Vector2.zero;
+
+        
+        unFullScreenBtn.SetActive(true);
+        fullScreenBtn.SetActive(false);
+
+        exitBtn.transform.localPosition = new Vector3(900, 405);
+    }
+
+    public void UnFullScreen()
+    {
+
+        RectTransform rect = GetComponent<Image>().rectTransform;
+
+        rect.anchorMin = halfVec;
+        rect.anchorMax = halfVec;
+        rect.pivot = halfVec;
+        rect.offsetMax = maxOffsetUnFull;
+        rect.offsetMin = minOffsetUnFull;
+        rect.localPosition = Vector3.zero;
+
+        unFullScreenBtn.SetActive(false);
+        fullScreenBtn.SetActive(true);
+
+        exitBtn.transform.localPosition = new Vector3(415, 205);
     }
 }
