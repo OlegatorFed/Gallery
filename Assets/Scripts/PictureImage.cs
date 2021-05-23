aededa1e971643457bbf337898b3fed0b391e89d
Oleg <fed.olegator1@gmail.com>
2021-05-23 10:46:14 +1000

    Implemented zoom

--- Assets/Scripts/PictureImage.cs
+++ Assets/Scripts/PictureImage.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PictureImage : MonoBehaviour
+public class PictureImage : MonoBehaviour, IDragHandler
 {
 
     public GameObject unFullScreenBtn;
     public GameObject fullScreenBtn;
+    public GameObject zoomInBtn;
+    public GameObject zoomOutBtn;
     public GameObject exitBtn;
+
+    bool zoomedIn = false;
 
     Vector2 maxOffsetUnFull = new Vector2(1000f, 0);
     Vector2 minOffsetUnFull = new Vector2(0, -450f);
     Vector2 halfVec = new Vector2(0.5f, 0.5f);
 
+    RectTransform rect;
+
     // Start is called before the first frame update
     void Awake()
     {
         gameObject.SetActive(false);
+        rect = GetComponent<Image>().rectTransform;
     }
 
     // Update is called once per frame
@@ -37,7 +45,7 @@ public class PictureImage : MonoBehaviour
 
     public void GoFullScreen()
     {
-        RectTransform rect = GetComponent<Image>().rectTransform;
+        
 
         rect.anchorMin = Vector2.zero;
         rect.anchorMax = Vector2.one;
@@ -48,6 +56,7 @@ public class PictureImage : MonoBehaviour
         
         unFullScreenBtn.SetActive(true);
         fullScreenBtn.SetActive(false);
+        zoomInBtn.SetActive(true);
 
         exitBtn.transform.localPosition = new Vector3(900, 405);
     }
@@ -66,7 +75,43 @@ public class PictureImage : MonoBehaviour
 
         unFullScreenBtn.SetActive(false);
         fullScreenBtn.SetActive(true);
+        zoomInBtn.SetActive(false);
 
         exitBtn.transform.localPosition = new Vector3(415, 205);
+    }
+
+    public void ZoomIn()
+    {
+        transform.localScale = new Vector3(2, 2);
+
+        zoomOutBtn.SetActive(true);
+        unFullScreenBtn.SetActive(false);
+        zoomInBtn.SetActive(false);
+        exitBtn.SetActive(false);
+
+        zoomedIn = !zoomedIn;
+    }
+     public void ZoomOut()
+    {
+        transform.localScale = new Vector3(1, 1);
+        transform.localPosition = Vector3.zero;
+
+
+        zoomOutBtn.SetActive(false);
+        unFullScreenBtn.SetActive(true);
+        zoomInBtn.SetActive(true);
+        exitBtn.SetActive(true);
+
+        zoomedIn = !zoomedIn;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (zoomedIn)
+        {
+            transform.position += (Vector3)eventData.delta;
+            transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, -959, 959), Mathf.Clamp(transform.localPosition.y, -417, 417));
+        }
+        
     }
 }
